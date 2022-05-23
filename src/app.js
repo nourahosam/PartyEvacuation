eb4573471fb81aa6f361b1280ca2a32cc04895a2
nourahosam <34795230+nourahosam@users.noreply.github.com>
2022-05-23 21:13:05 +0200

    Additions to frontend

--- src/app.js
+++ src/app.js
@@ -3,12 +3,24 @@ const app = express();
 const Database  = require('./utilities/dbConnection');
 const cors = require('cors');
 const mongoose = require('mongoose');
-
+const cookieParser = require('cookie-parser');
+const session = require('express-session');
 
 const router = require('./api/routes');
 
 app.use(cors());
 app.use(express.json());
+
+// app.use(cookieParser());
+// app.use(session({
+//     key: "",
+//     secret: "",
+//     resave: false,
+//     saveUninitialized: false,
+//     cookie: {
+//         expires: 60
+//     }
+// }))
 app.use(router);
 
 
